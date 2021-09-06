c43431df1ac4acff3d465d1cd1f1e49bc063660d
dev <dev@breadslice.de>
2021-09-06 18:41:58 +0200

    live versoin

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1176,7 +1176,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 48, 14, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1366,7 +1366,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 380				// MODIFIED
+#define X_BED_SIZE 368				// MODIFIED
 #define Y_BED_SIZE 250				// MODIFIED
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
@@ -1531,7 +1531,7 @@
  *   leveling in steps so you can manually adjust the Z height at each grid-point.
  *   With an LCD controller the process is guided step-by-step.
  */
-//#define AUTO_BED_LEVELING_3POINT
+#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 //#define AUTO_BED_LEVELING_UBL
